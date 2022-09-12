5d32995eb208d2eec8a4d49643ffb4e478cb70e3
DenisBokov <57086505+DenisBokov@users.noreply.github.com>
2022-09-12 14:22:58 +0300

    Initial Commit

---
+++ ChangeScreenColorWithSwiftUI/ChangeScreenColorWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeScreenColorWithSwiftUIApp.swift
+//  ChangeScreenColorWithSwiftUI
+//
+//  Created by Denis Bokov on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChangeScreenColorWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
